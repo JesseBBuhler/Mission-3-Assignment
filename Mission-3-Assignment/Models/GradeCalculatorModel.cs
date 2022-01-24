012d12d78c55c3c617c2d436cd2dcfedf06afd97
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2022-01-24 06:56:11 -0700

    Adding models

---
+++ Mission-3-Assignment/Models/GradeCalculatorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_3_Assignment.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Range(1, 100)]
+        public int assign { get; set; }
+        [Range(1, 100)]
+        public int group { get; set; }
+        [Range(1, 100)]
+        public int quiz { get; set; }
+        [Range(1, 100)]
+        public int exam { get; set; }
+        [Range(1, 100)]
+        public int intex { get; set; }
+    }
+}
